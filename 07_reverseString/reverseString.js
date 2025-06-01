331e3a615b9a619679fb765086d6a65f44adaec8
codingMilad <miladybadria@gmail.com>
2025-06-01 17:10:11 +0200

    adding exercises 6-11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const sentence = string.split("").reverse()
+    return sentence.join("")
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = []
+    for(let i = 0; i <= array.length; i++) {
+        if (!remove.includes(array[i])) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
 };
-
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
