13559ffac1509a9205077ca34641748c02439560
codingMilad <miladybadria@gmail.com>
2025-06-29 21:13:42 +0200

    palindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let j = word.length - 1;
 
+    const isAlphanumeric = (char) => {
+        return (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9');
+    };
+
+    for (let i = 0; i < word.length; i++) {
+        if (!isAlphanumeric(word[i])) {
+            continue;
+        }
+
+        while (j >= 0 && !isAlphanumeric(word[j])) {
+            j--;
+        }
+
+        if (i >= j) {
+            return true;
+        }
+
+        if (word[i] !== word[j]) {
+            return false;
+        }
+
+        j--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
