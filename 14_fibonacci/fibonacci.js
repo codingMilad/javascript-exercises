f7e72d33dbb10bb4ff0bbbdfc2bd32e86efb015d
codingMilad <miladybadria@gmail.com>
2025-07-04 20:02:54 +0200

    fibonacci was fun

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number)
+    feb = [1,1]
+    if (number > 2) {
+        for (let i = 0; i < number; i++) {
+            if ( i > 1) {
+                let hold = (feb[i - 2]) + (feb[i - 1])
+                feb.push(hold)
+            } else {
+                continue
+            }
+        }
+        
+    } else if(number === 1 || number === 2) {
+        return 1
+    } else if(number < 0) {
+        return "OOPS"
+    } else {
+        return 0
+    }
+    return feb[number-1]
 
 };
 
