2620ca827ef80cdc64fd07158ce8c61bd7bc1775
codingMilad <miladybadria@gmail.com>
2025-07-06 14:56:49 +0200

    learned new Date().getFullYear)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = null;
+    let maxAge = 0;
+    const currentYear = new Date().getFullYear();
 
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        const yearOfDeath = person.yearOfDeath || currentYear; 
+        const age = yearOfDeath - person.yearOfBirth;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
